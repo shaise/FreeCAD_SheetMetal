573c04dfdf22aedd9b6c65d1b92df6a633664f89
Alexandre Prokoudine <alexandre.prokoudine@gmail.com>
2024-01-26 13:34:04 +0100

    Change Library to Lesser

--- engineering_mode.py
+++ engineering_mode.py
@@ -6,7 +6,7 @@
 #  Copyright 2019 Cerem Cem Aslan
 #
 #  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Library General Public
+#  modify it under the terms of the GNU Lesser General Public
 #  License as published by the Free Software Foundation; either
 #  version 2 of the License, or (at your option) any later version.
 #
@@ -15,7 +15,7 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU Library General Public
+#  You should have received a copy of the GNU Lesser General Public
 #  License along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
--- translations/compile_qm.py
+++ translations/compile_qm.py
@@ -12,9 +12,9 @@
 #*   This program is distributed in the hope that it will be useful,       *
 #*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
 #*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-#*   GNU Library General Public License for more details.                  *
+#*   GNU Lesser General Public License for more details.                  *
 #*                                                                         *
-#*   You should have received a copy of the GNU Library General Public     *
+#*   You should have received a copy of the GNU Lesser General Public     *
 #*   License along with this program; if not, write to the Free Software   *
 #*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
 #*   USA                                                                   *
