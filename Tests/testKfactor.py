1395d59de42eabd765fd8cd2bb80905ec6fb8404
sliptonic <shopinthewoods@gmail.com>
2024-03-30 09:25:42 -0500

    bend tests

--- Tests/testKfactor.py
+++ Tests/testKfactor.py
@@ -1,7 +1,7 @@
 # #######################################################################
-#  
+#
 #  Copyright (c) 2023 Ondsel Inc.
-#  
+#
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
 #  License as published by the Free Software Foundation; either
