cccb03d998ccfd3fec86168b0966a53adcbf9275
sheetmetalman <lucasmartinsmac@gmail.com>
2024-11-22 09:36:02 -0300

    Remove duplicate lines

--- ExtrudedCutout.py
+++ ExtrudedCutout.py
@@ -368,16 +368,6 @@ if SheetMetalTools.isGuiLoaded():
         def __setstate__(self, state):
             '''Restore the object from its state'''
             self.loads(state)
-        
-        def dumps(self):
-            return None
-
-        def loads(self, state):
-            if state is not None:
-                import FreeCAD
-
-                doc = FreeCAD.ActiveDocument  # crap
-                self.Object = doc.getObject(state["ObjectName"])
 
         # dumps and loads replace __getstate__ and __setstate__ post v. 0.21.2
         def dumps(self):
